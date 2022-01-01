90e8566b0e791a1d58dad9940e2518cc48b69486
Carlos Salgado <charlie@csalgado.io>
2022-01-01 01:27:14 +0100

    wip

--- model.py
+++ model.py
@@ -2,7 +2,12 @@ from dataclasses import dataclass
 
 import re
 import markdown
+import toml
 
+with open('config.toml') as file:
+    config = toml.load(file)
+
+CACHE_ADDRESS = config['cache_address']
 
 @dataclass
 class Card:
@@ -16,13 +21,17 @@ class Card:
           raise Exception('No lines received!')
           
       title = card[0].strip('! ') if re.match(r"^! .*", card[0]) else None
-      images = re.findall(r"^http.*", ''.join(card).strip('\n'))
+      images = []
+      for line in card:
+          if re.findall(r"^http.*",line) or re.findall(r".*(jpg|png)",line):
+              images.append(line)
+
       image = None
       if images:
           image = images[0]
-      body = None
-      # If we don't have an image, then the card is title + body type card
-      if not image:
+          body = "\n".join(card[1:])
+          return cls(title, body, image)
+      else:
           # Edge case: Card with single line body and no title
           if (len(card) == 1) and (not title):
               body = card[0]
@@ -34,12 +43,16 @@ class Card:
 
 
     def render(self):
-      if self.image:
-          return f"<img src='{self.image}'>"
-      else:
-          body = ""
-          if self.body:
-              body = markdown.markdown(self.body)
+        body = ""
+        if self.body:
+          body = markdown.markdown(self.body)
+        if self.image:
+          address = f"{CACHE_ADDRESS}/{self.image}"
+          if not re.match(r"^http.*", self.image):
+              address = f'{CACHE_ADDRESS}/files/{self.image}'
+
+          return f"<img src='{address}'><div>{body}</div>"
+        else:
           title = ""
           if self.title:
               newline = '\n'
